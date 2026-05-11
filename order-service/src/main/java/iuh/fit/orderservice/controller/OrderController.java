40c95950f4d8bb1fd477dc6f4588565a234c65ef
dgkhoa279 <vodangkhoa270904@gmail.com>
2026-05-11 22:15:59 +0700

    feat(order): query order

--- order-service/src/main/java/iuh/fit/orderservice/controller/OrderController.java
+++ order-service/src/main/java/iuh/fit/orderservice/controller/OrderController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/orders")
 public class OrderController {
@@ -21,5 +23,15 @@ public class OrderController {
     public ResponseEntity<OrderResponse> createOrder(@RequestBody CreateOrderRequest request) {
         OrderResponse response = orderService.createOrder(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderResponse> getOrderById(@PathVariable String orderId) {
+        return ResponseEntity.ok(orderService.getOrderById(orderId));
+    }
+
+    @GetMapping("/customer/{customerId}")
+    public ResponseEntity<List<OrderResponse>> getOrdersByCustomerId(@PathVariable String customerId) {
+        return ResponseEntity.ok(orderService.getOrdersByCustomerId(customerId));
     }
 }
--- order-service/src/main/java/iuh/fit/orderservice/repo/OrderRepository.java
+++ order-service/src/main/java/iuh/fit/orderservice/repo/OrderRepository.java
@@ -3,8 +3,11 @@ package iuh.fit.orderservice.repo;
 import iuh.fit.orderservice.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
     boolean existsByOrderCode(String orderCode);
+
+    List<Order> findByCustomerIdOrderByOrderDateDesc(UUID customerId);
 }
